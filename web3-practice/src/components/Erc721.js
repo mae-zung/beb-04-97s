196076eec08d5d63050b7b3d7343874034b77625
mae-zung <candylim97@ewhain.net>
2022-06-14 14:52:28 +0900

    transfer

--- web3-practice/src/components/Erc721.js
+++ web3-practice/src/components/Erc721.js
@@ -1,13 +1,47 @@
-function Erc721({ erc721list }) {
+import erc721Abi from "../erc721Abi";
+import { useState } from "react";
+
+function Erc721({ web3, account, erc721list }) {
+  const [to, setTo] = useState("");
+  const sendToken = async (tokenAddr, tokenId) => {
+    console.log(tokenAddr + "," + tokenId);
+    const tokenContract = await new web3.eth.Contract(erc721Abi, tokenAddr, {
+      from: account,
+    });
+    tokenContract.methods
+      .transferFrom(account, to, tokenId)
+      .send({
+        from: account,
+      })
+      .on("receipt", (receipt) => {
+        setTo("");
+      });
+  };
   return (
-    <div>
+    <div className="erc721list">
       {erc721list.map((token) => {
         return (
           <div className="erc721token">
             Name: <span className="name">{token.name}</span>(
             <span className="symbol">{token.symbol}</span>)
             <div className="nft">id: {token.tokenId}</div>
-            <img src={token.tokenURI} alt="nft" width={300} />
+            <img src={token.tokenURI} width={300} />
+            <div className="tokenTransfer">
+              To:{" "}
+              <input
+                type="text"
+                value={to}
+                onChange={(e) => {
+                  setTo(e.target.value);
+                }}
+              ></input>
+              <button
+                className="sendErc20Btn"
+                onClick={sendToken.bind(this, to, token.tokenId)}
+              >
+                send Token
+              </button>
+            </div>
           </div>
         );
       })}
